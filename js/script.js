8ca46837e7847f3fc00dad345116be9d562efa8f
Hilol <khilol2017@gmail.com>
2024-01-16 21:00:11 +0300

    final

--- js/script.js
+++ js/script.js
@@ -1,14 +1,63 @@
-// Находим все изображения внутри элемента с классом "sub__main"
+// Смена картин в карточке товара
 const subImages = document.querySelectorAll('.sub__main img');
-
-// Находим элемент с классом "main", в который мы будем вставлять выбранные изображения
 const mainImage = document.querySelector('.main img');
 
-// Добавляем обработчик события клика для каждого изображения в ".sub__main"
 subImages.forEach((image, index) => {
   image.addEventListener('click', () => {
-    // При клике заменяем изображение в элементе с классом "main" на изображение, которое было выбрано
-    mainImage.src = image.src; // Заменяем src изображения в .main на src изображения, по которому кликнули
-    mainImage.alt = image.alt; // Меняем alt атрибут для доступности
+    
+    mainImage.src = image.src; 
+    mainImage.alt = image.alt; 
   });
+});
+
+// Смена картин
+const cards = document.querySelectorAll('.wrapper .card');
+
+cards.forEach(card => {
+  const cardImage = card.querySelector('.img-card');
+  card.style.backgroundSize = 'cover';
+  const originalImageSrc = cardImage.src;
+
+  const newImageSrc = './img/img-2.jpg';
+  card.addEventListener('mouseenter', function() {
+    cardImage.src = newImageSrc; 
+  });
+
+  card.addEventListener('mouseleave', function() {
+    cardImage.src = originalImageSrc;
+  });
+});
+
+// Корзина
+const cartButtons = document.querySelectorAll('.cart-btn');
+const cartModal = document.querySelector('.cart-modal');
+const closeBtn = cartModal.querySelector('.close');
+cartButtons.forEach(button => {
+  button.addEventListener('click', function() {
+    cartModal.style.display = 'block';
+
+  });
+});
+
+closeBtn.addEventListener('click', function() {
+  cartModal.style.display = 'none';
+});
+
+// Добавление в корзину
+const cartContent = cartModal.querySelector('.cart-content');
+
+cards.forEach(card => {
+  const cartBtn = card.querySelector('.cart-btn');
+  cartBtn.addEventListener('click', () => {
+    const selectedCard = card.cloneNode(true);
+    const selectedCartBtn = selectedCard.querySelector('.cart-btn');
+    selectedCartBtn.remove(); // Удаляем кнопку из скопированного блока
+    cartContent.appendChild(selectedCard);
+    cartModal.style.display = 'block';
+  });
+});
+
+closeBtn.addEventListener('click', () => {
+  cartContent.innerHTML = '';
+  cartModal.style.display = 'none';
 });
